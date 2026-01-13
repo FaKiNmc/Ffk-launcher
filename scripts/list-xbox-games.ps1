ce1b7b525551cea3b1dc6b7927736ef930959cf8
FaKiNmc <contactaconfakin@gmail.com>
2026-01-13 22:25:56 +0100

    chore: remove source code, keep only README for release distribution

--- scripts/list-xbox-games.ps1
+++
@@ -1,5 +0,0 @@
-Get-AppxPackage -AllUsers | ForEach-Object {
-    if ($_.InstallLocation -and $_.InstallLocation -notlike "*WindowsApps*") {
-        Write-Output "$($_.Name) | $($_.InstallLocation) | $($_.PackageFamilyName)"
-    }
-}
